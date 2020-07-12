c4812d5377a16380546b5f282454c54dd322c14f
FerdinandWang <603334301@qq.com>
2020-07-12 22:52:21 +0800

    Create [77]组合.java

---
+++ Week_03/[77]组合.java
@@ -0,0 +1,50 @@
+//给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。 
+//
+// 示例: 
+//
+// 输入: n = 4, k = 2
+//输出:
+//[
+//  [2,4],
+//  [3,4],
+//  [2,3],
+//  [1,2],
+//  [1,3],
+//  [1,4],
+//] 
+// Related Topics 回溯算法 
+// 👍 310 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    List<List<Integer>> output = new LinkedList();
+    int n;
+    int k;
+
+    public void backtrack(int first, LinkedList<Integer> curr) {
+        // if the combination is done
+        if (curr.size() == k)
+            output.add(new LinkedList(curr));
+
+        for (int i = first; i < n + 1; ++i) {
+            // add i into the current combination
+            curr.add(i);
+            // use next integers to complete the combination
+            backtrack(i + 1, curr);
+            // backtrack
+            curr.removeLast();
+        }
+    }
+
+    public List<List<Integer>> combine(int n, int k) {
+        this.n = n;
+        this.k = k;
+        backtrack(1, new LinkedList<Integer>());
+        return output;
+    }
+}
+//runtime:18 ms
+//memory:42.6 MB
+
+//leetcode submit region end(Prohibit modification and deletion)
